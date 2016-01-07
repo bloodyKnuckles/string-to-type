d771db9bb053f53f186453777618ce5b266b4f93
bloodyKnuckles <treenopie@gmail.com>
2016-01-07 12:01:59 -0600

    added test and examples

--- README.md
+++ README.md
@@ -3,6 +3,18 @@
 Given a string variable, this module detects the if the string is a stringified boolean or numeric
 value, then converts the variable and returns it.
 
+# example
+
+```
+var var typeDetectConvert = require('string-to-type')
+
+console.log(typeof typeDetectConvert("true"))
+// boolean
+
+console.log(typeof typeDetectConvert("1234"))
+// number
+```
+
 # license
 
 MIT
--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ var typeDetectConvert = require('../')
 var test = require('tape')
 
 test('detect string value type and convert', function (tt) {
-    tt.plan(4)
+    tt.plan(6)
 
     var str = 'true'
     var result = true
@@ -11,10 +11,12 @@ test('detect string value type and convert', function (tt) {
     var str = 'false'
     var result = false
     tt.equal(typeDetectConvert(str), result)
+    tt.equal(typeof typeDetectConvert(str), 'boolean')
 
     var str = '4'
     var result = 4 
     tt.equal(typeDetectConvert(str), result)
+    tt.equal(typeof typeDetectConvert(str), 'number')
 
     var str = '1234'
     var result = 1234 
